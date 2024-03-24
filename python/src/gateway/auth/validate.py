eafc5abc85659d16f8749c7c6705a37a08ebbb41
Arya Vats <aryavats123@gmail.com>
2024-03-25 03:12:04 +0530

    add gateway and rabbitMQ

--- python/src/gateway/auth/validate.py
+++ python/src/gateway/auth/validate.py
@@ -4,3 +4,14 @@ def token(requests):
 	if not "Authorization" in request.headers:
 		return "misisng credentials", 401
 	token = request.headers["Authorization"]
+
+	if not token:
+		return None, ("missing credentials", 401)
+	response = requests.post(
+		f"http://{os.environ.get('AUTH_SVC_ADDRESS')}/validate"
+	)	headers={"Authorization":token}
+
+	if response.status_code == 200:
+		return response.txt, None
+	else:
+		return None, (response.txt, response.status_code)	
